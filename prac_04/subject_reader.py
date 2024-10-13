b8d1f60f8821f2e672b6e752e5a4156275bacc9f
milaang <mila.ang@my.jcu.edu.au>
2024-10-13 18:31:07 +0800

    Added an empty list that stores data from the file, append the data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -12,18 +12,17 @@ def main():
 
 
 def load_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+    """Read data from file formatted like: subject, lecturer, number of students."""
+    data_list = []  # Create an empty list to store the data
+
+    with open(FILENAME) as input_file:
+        for line in input_file:
+            line = line.strip()  # Remove the \n at the end of each line
+            parts = line.split(',')  # Separate the data into its parts
+            parts[2] = int(parts[2])  # Convert the number of students to an integer
+            data_list.append(parts)  # Append the processed line (list) to the main list
+
+    return data_list  # Return the list of lists
 
 
 main()
