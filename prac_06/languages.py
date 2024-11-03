cb25fc7e8153020de2ab6d7f50acedc853d98d75
milaang <mila.ang@my.jcu.edu.au>
2024-11-03 20:45:05 +0800

    Completed languages program

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -12,10 +12,6 @@ def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-    print(python)
-    print(ruby)
-    print(visual_basic)
-
     languages = [python, ruby, visual_basic]
     for language in languages:
         print(language)
