4956a7915a39de082c85379afb16374072ca8271
milaang <mila.ang@my.jcu.edu.au>
2024-11-03 19:30:08 +0800

    Added COLOR_CODES dictionary with color names and hexadecimal values

--- prac_05/hex_colors.py
+++ prac_05/hex_colors.py
@@ -3,3 +3,16 @@ CP1404 - Practical
 Hex Colors
 """
 
+# Dictionary of color names and their hexadecimal values
+COLOR_CODES = {
+    "AliceBlue": "#f0f8ff",
+    "AntiqueWhite": "#faebd7",
+    "Aqua": "#00ffff",
+    "Aquamarine": "#7fffd4",
+    "Azure": "#f0ffff",
+    "Beige": "#f5f5dc",
+    "Bisque": "#ffe4c4",
+    "Black": "#000000",
+    "BlanchedAlmond": "#ffebcd",
+    "Blue": "#0000ff"
+}
