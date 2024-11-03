cafcdb1abbed509b045727e56a1230d1ffaace28
milaang <mila.ang@my.jcu.edu.au>
2024-11-03 20:39:32 +0800

    Create ProgrammingLanguage class with init and is_dynamic methods

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,3 +4,18 @@ Programming Language
 Estimate: 15 minutes
 Actual:
 """
+
+
+class ProgrammingLanguage:
+    """Represent a programming language with typing, reflection, and year of origin."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize the programming language with its attributes."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return True if the programming language is dynamically typed."""
+        return self.typing == "Dynamic"
