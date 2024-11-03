1d3fe51dfaafe86594531f72f71b335cc4facb6e
milaang <mila.ang@my.jcu.edu.au>
2024-11-03 20:41:21 +0800

    Add languages.py to create and display programming language objects

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,3 +2,19 @@
 CP1404 - Practical
 Languages (Client Code)
 """
+
+from programming_language import ProgrammingLanguage
+
+
+def main():
+    """Create and display programming language objects."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
+    print(ruby)
+    print(visual_basic)
+
+
+main()
