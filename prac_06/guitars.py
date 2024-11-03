67d8d32d785e59aa6079336d1c301076be57640f
milaang <mila.ang@my.jcu.edu.au>
2024-11-03 21:14:37 +0800

    Collecting and displaying guitar details, with vintage indication

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,3 +4,32 @@ Guitars
 Estimate: 15 minutes
 Actual:
 """
+
+from guitar import Guitar
+
+
+def main():
+    """Collect and display a list of guitars."""
+    guitars = []
+    print("My guitars!")
+
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(f"{new_guitar} added.")
+        name = input("Name: ")
+
+    # Pre-fill with sample guitars for demonstration
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
