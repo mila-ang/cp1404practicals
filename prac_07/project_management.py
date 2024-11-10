9840861e722e548efcd6e0a8753a1b9ddbbb9050
milaang <mila.ang@my.jcu.edu.au>
2024-11-10 22:28:22 +0800

    Implement load_projects function to read projects from a file

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,3 +4,17 @@ Project Management
 Estimate: 1 hour
 Actual:
 """
+
+from project import Project
+
+
+def load_projects(filename):
+    """Load projects from a file and return a list of Project objects."""
+    projects = []
+    with open(filename, 'r') as file:
+        file.readline()  # Skip the header
+        for line in file:
+            parts = line.strip().split('\t')
+            project = Project(parts[0], parts[1], parts[2], parts[3], parts[4])
+            projects.append(project)
+    return projects
