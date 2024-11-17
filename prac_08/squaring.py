2b7efb341460d0e0ece67f3ff8ccdff5f7fc79af
milaang <mila.ang@my.jcu.edu.au>
2024-11-17 18:27:25 +0800

    Adjust SquareNumberApp

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -13,21 +13,21 @@ __author__ = 'Lindsay Ward'
 
 
 class SquareNumberApp(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
+    """SquareNumberApp is a Kivy App for squaring a number"""
     def build(self):
-        """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
-        self.title = "Square Number"
+        """Build the Kivy app from the kv file"""
+        Window.size = (400, 200)
+        self.title = "Square Number 2"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
     def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
+        """Handle calculation (could be button press or other call), output result to label widget"""
         try:
             result = float(value) ** 2
             self.root.ids.output_label.text = str(result)
         except ValueError:
-            pass
+            self.root.ids.output_label.text = "Invalid input"
 
 
 SquareNumberApp().run()
