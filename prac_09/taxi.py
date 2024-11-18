0a26236f25640f2e991eecc20a4a13954ec8fe33
milaang <mila.ang@my.jcu.edu.au>
2024-11-18 15:09:40 +0800

    Update get_fare() method to round fare to the nearest 10c

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -21,8 +21,9 @@ class Taxi(Car):
         return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
 
     def get_fare(self):
-        """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        """Return the price for the taxi trip, rounded to the nearest 10c."""
+        fare = self.price_per_km * self.current_fare_distance
+        return round(fare, 1)  # Rounds to the nearest 10c
 
     def start_fare(self):
         """Begin a new fare."""
