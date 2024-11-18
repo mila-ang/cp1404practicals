095ae837ce8c7469c0d5558ba651ee33dd39cae1
milaang <mila.ang@my.jcu.edu.au>
2024-11-18 14:22:28 +0800

    Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,11 @@
+"""
+CP1404 - Practical
+Taxi Test
+"""
+
+from taxi import Taxi
+
+
+def main():
+    # Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
+    my_taxi = Taxi("Prius 1", 100, 1.23)
