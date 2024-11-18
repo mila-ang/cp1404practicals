e6b30eebe143e66e24fa0fb6138a682b3b528334
milaang <mila.ang@my.jcu.edu.au>
2024-11-18 14:34:10 +0800

    Update so it does not pass price_per_km when creating a taxi instance

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -8,7 +8,7 @@ from taxi import Taxi
 
 def main():
     # Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
 
     # Drive taxi 40km
     my_taxi.drive(40)
