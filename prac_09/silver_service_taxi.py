abcf58aa5fbfb64c01d2c5a665043221cf2c8ab6
milaang <mila.ang@my.jcu.edu.au>
2024-11-18 15:02:29 +0800

    Create a new class for a SilverServiceTaxi that inherits from Taxi, with a new "fanciness" attribute

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,3 +2,14 @@
 CP1404 - Practical
 Silver Service Taxi
 """
+
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes fanciness."""
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi instance."""
+        super().__init__(name, fuel)
+        self.price_per_km = Taxi.price_per_km * fanciness  # Scale price per km
+        self.fanciness = fanciness
