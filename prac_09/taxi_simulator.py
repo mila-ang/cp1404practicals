146a61120d7dad12a8c5ed19c849875716b1c352
milaang <mila.ang@my.jcu.edu.au>
2024-11-18 15:25:26 +0800

    Set up basic structure for taxi simulator with menu loop

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -2,3 +2,28 @@
 CP1404 - Practical
 Taxi Simulator
 """
+
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MENU = ("q)uit, "
+        "c)hoose taxi, "
+        "d)rive")
+
+
+def main():
+    """Simulate a taxi ride with options to choose taxis, drive, and view the bill."""
+    print("Let's drive!")
+    print(MENU)
+
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice not in ["c", "d"]:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").lower()
+
+    print("Goodbye!")
+
+
+main()
