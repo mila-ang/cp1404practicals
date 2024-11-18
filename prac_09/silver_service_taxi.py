3b24eebf9cd8c19534dc8d11693b991ba6c7a1e6
milaang <mila.ang@my.jcu.edu.au>
2024-11-18 15:04:45 +0800

    Add "flagfall" as a class variable, and add __str__ method, and get_fare method

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -8,8 +8,19 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Specialised version of a Taxi that includes fanciness."""
+
+    flagfall = 4.50
+
     def __init__(self, name, fuel, fanciness):
         """Initialise a SilverServiceTaxi instance."""
         super().__init__(name, fuel)
         self.price_per_km = Taxi.price_per_km * fanciness  # Scale price per km
         self.fanciness = fanciness
+
+    def __str__(self):
+        """Return a string representation including flagfall."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip, including flagfall."""
+        return super().get_fare() + self.flagfall
